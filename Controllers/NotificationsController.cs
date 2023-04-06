a10a38ed08cc3ddca02b655b3c409186a3e9ff11
unknown <santiago.1701713773@ucaldas.edu.co>
2023-04-06 18:57:38 -0500

    Envio correo electronico con 2fa

--- Controllers/NotificationsController.cs
+++ Controllers/NotificationsController.cs
@@ -65,13 +65,39 @@ public class NotificationsController : ControllerBase
         }
     }
 
-    private SenGridMessage CrearMensajeBase(ModeloCorreo datos)
+    
+    [Route("correo-correo-2fa")]
+    [HttpPost]
+    public async Task<ActionResult> EnivarCorreo2fa(ModeloCorreo datos)
     {
-        
+        var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+        var client = new SendGridClient(apiKey);
+        SendGridMessage msg = this.CrearMensajeBase(datos);
+        msg.SetTemplateId(Environment.GetEnvironmentVariable("SENGRID_2FA_SENGRID_TEMPLATE_ID"));
+        msg.SetTemplateData(new
+        {
+            name = datos.correoDestino,
+            message = datos.contenidoCorreo,
+            asunto = datos.asuntoCorreo
+        });
+        var response = await client.SendEmailAsync(msg);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted)
+        {
+            return Ok("correo enviado mensaje a la direccion" + datos.correoDestino);
+        }
+        else
+        {
+            return BadRequest("Error enviando mensaje a la direccion" + datos.correoDestino);
+        }
+    }
+
+    private SendGridMessage CrearMensajeBase(ModeloCorreo datos)
+    {   
         var from = new EmailAddress(Environment.GetEnvironmentVariable("EMAIL_FROM"), Environment.GetEnvironmentVariable("NAME_FROM"));
         var subject = datos.asuntoCorreo;
         var to = new EmailAddress(datos.correoDestino, datos.nombreDestino);
-        var plainTextContent = "plain text content";
+        var plainTextContent = datos.contenidoCorreo;
         var htmlContent = datos.asuntoCorreo;
         var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         return msg;
